e01754045a79f4267c55eabd99b7fa97707a8eb6
EdgarSnow75 <edgarsnow572000@gmail.com>
2023-08-25 16:02:34 +0630

    Added Cart Button and some styles

--- SwiftBuy/src/components/Common/Header.tsx
+++ SwiftBuy/src/components/Common/Header.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import classes from './Header.module.css';
+import CartButton from "../../Cart/CartButton";
 
 
 const Header: React.FC = () => {
     return (
         <header className={classes.navbar}>
             <div><text className={classes.logoTxt}>SwiftBuy</text></div>
-            <div className={classes.actionContainer}>
-                <button className={classes.cartBtn}>My Cart <span className={classes.cartCounter}>0</span></button>
-            </div>
+            <CartButton />
         </header>
     )
 }
